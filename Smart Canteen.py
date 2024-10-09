bfbec0e701de176a24d53b00b8cd1a7791fb582a
unknown <varunsinghrj4@gmail.com>
2024-10-09 12:19:05 +0530

    second commit

--- Smart Canteen.py
+++ Smart Canteen.py
@@ -1,9 +1,19 @@
 print(" -----Welcome To Smart Canteen-----\n")
 entry = input("Do You Want To Enter In Program (y/n):")
 if(entry=='y'):
-    print("1.MENU CHART")
-    x = int(input("Enter Your Entry:"))    
+    print("1.MENU CHART\n2.COMPLAIN\n3.FEEDBACK\n4.Exit From Program\n")
+    x = int(input("Enter Your Entry:"))
+        if(x==1):
 
 
-else:
+
+
+            
+            printf("Thank You")
+        if(x==2):
+            cmplain=input("\nRegister Your Complain:\n")
+
+        if(x==4):
+            print("Thank You For Visiting")
+            exit()
     exit()
