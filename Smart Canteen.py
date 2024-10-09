ec9478d60c92c7f58471cc41b61da529f23389ff
unknown <varunsinghrj4@gmail.com>
2024-10-09 11:33:50 +0530

    first commit

---
+++ Smart Canteen.py
@@ -0,0 +1,9 @@
+print(" -----Welcome To Smart Canteen-----\n")
+entry = input("Do You Want To Enter In Program (y/n):")
+if(entry=='y'):
+    print("1.MENU CHART")
+    x = int(input("Enter Your Entry:"))    
+
+
+else:
+    exit()
